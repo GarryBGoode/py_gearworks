1ba7caec2dd3a3b0e4a066b98cd2abc2947085cc
BencsikG <bgeri91@gmail.com>
2026-08-30 18:38:59 +0200

    License notes standardized, bumped to 2026. pyproject.toml improvements

--- examples/planetary_drives.py
+++ examples/planetary_drives.py
@@ -1,3 +1,15 @@
+# Copyright 2026 Gergely Bencsik
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 from py_gearworks import *
 from ocp_vscode import show, Animation
 from copy import deepcopy
@@ -80,6 +92,7 @@ gear_ring_track = np.linspace(0, -gear_ring.pitch_angle * 180 / np.pi, n + 1) * 
 gear_planet_track = (
     np.linspace(0, -gear_planet.pitch_angle * 180 / np.pi, n + 1) * duration
 )
+show(gear_drive)
 # assign the tracks to the gears
 animation = Animation(gear_drive)
 animation.add_track("/gear_drive/gear_sun", "rz", time_track, gear_sun_track)
@@ -90,5 +103,5 @@ for i in range(num_planets):
     )
 # animation.add_track("/gear_drive", "rz", time_track, -gear_ring_track)
 
-show(gear_drive)
+
 animation.animate(speed=1)
